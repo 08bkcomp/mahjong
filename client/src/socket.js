560e4c97c254a7e1b8856ecfc991d39b69924332
Balaji Krishna <08bkcomp@gmail.com>
2019-03-21 12:41:06 +0000

    try no param client sock

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,5 @@
 import openSocket from 'socket.io-client';
 var port = process.env.PORT || 5000;
-const socket = openSocket('http://localhost:'+port);
+//const socket = openSocket('http://localhost:'+port);
+const socket = openSocket();
 export default socket;
