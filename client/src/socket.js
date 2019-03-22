461aab4b5bd12a7ae32d02dc3f36e1acd3033d9e
Balaji Krishna <08bkcomp@gmail.com>
2019-03-22 22:48:33 +0000

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,11 +1,7 @@
 import openSocket from 'socket.io-client';
 var port = process.env.PORT || 5000;
 let socket;
-var heroku = true;
-if (heroku) {
-  const socket = openSocket('https://mahjong-balaji.herokuapp.com');
-} else {
-  const socket = openSocket('http://localhost:' + port);
-}
+const socket = openSocket('https://mahjong-balaji.herokuapp.com');
+//const socket = openSocket('http://localhost:' + port);
 //const socket = openSocket('https://mahjong-balaji.firebaseapp.com');
 export default socket;
