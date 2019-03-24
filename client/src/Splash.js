a8a80a4d347be12380ceb322ccb9db53fd3d28fd
Balaji Krishna <08bkcomp@gmail.com>
2019-03-24 23:05:08 +0000

    firebase google auth working

--- client/src/Splash.js
+++ client/src/Splash.js
@@ -1,78 +1,65 @@
 import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom';
-import socket from './socket';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import socket from './socket';
+
+var firebase = require('firebase');
+var config = {
+  apiKey: 'AIzaSyB2RDY7MVXSuWZj73ZZhX4AWo5phJKPjek',
+  authDomain: 'mahjong-user-auth.firebaseapp.com',
+  databaseURL: 'https://mahjong-user-auth.firebaseio.com',
+  projectId: 'mahjong-user-auth',
+  storageBucket: 'mahjong-user-auth.appspot.com',
+  messagingSenderId: '188181150298',
+};
+firebase.initializeApp(config);
+var provider = new firebase.auth.GoogleAuthProvider();
 
 export default class Splash extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      playerCreated: false,
-      username: null,
-      usernameValid: false,
-    };
-    socket.on('username already exists', () => {
-      this.setState({usernameValid: true});
-    });
-    socket.on('username created', () => {
-      document.removeEventListener('keydown', this.enterHandler);
-      this.setState({playerCreated: true});
-    });
-
-    document.addEventListener('keydown', this.enterHandler);
+    this.state = {playerCreated: false, redirect: false};
+    setInterval(() => {
+      console.log(this.state);
+    }, 5000);
+    this.redirectToLogin = this.redirectToLogin.bind(this);
   }
-  validateUsername = username => {
-    if (username === null) {
-      return false;
-    }
-    if (username.length < 5) {
-      return false;
-    }
-    return true;
-  };
 
-  enterHandler = event => {
-    console.log(event.key);
-    if (event.key === 'Enter' && this.validateUsername(this.state.username)) {
-      this.createUser();
-    } else {
-      this.setState({
-        usernameValid: this.validateUsername(this.state.username),
+  redirectToLogin = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        console.log('user data from google');
+        console.log(result.user);
+        socket.emit('create player', result.user.email);
+        this.setState({playerCreated: true});
+        setTimeout(() => {
+          this.setState({redirect: true});
+        }, 1500);
+      })
+      .catch(error => {
+        console.log(error);
       });
-    }
-  };
-
-  handleNameChange = event => {
-    this.setState({username: event.target.value, usernameValid: false});
-  };
-
-  createUser = () => {
-    socket.emit('create player', this.state.username);
   };
 
   render() {
-    if (this.state.playerCreated) {
+    if (this.state.redirect) {
       return <Redirect to="/lobby" />;
     }
+    if (this.state.playerCreated) {
+      return (
+        <Alert>
+          <Alert.Heading>Redirecting to lobby...</Alert.Heading>
+        </Alert>
+      );
+    }
     return (
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="Username"
-          value={this.state.username}
-          onChange={this.handleNameChange}
-        />
-        <InputGroup.Append>
-          {this.state.usernameValid ? (
-            <Button variant="outline-danger">Invalid Username</Button>
-          ) : (
-            <Button variant="success" onClick={this.createUser}>
-              Create User
-            </Button>
-          )}
-        </InputGroup.Append>
-      </InputGroup>
+      <Alert>
+        <Alert.Heading>Please login with provider...</Alert.Heading>
+        <Button onClick={this.redirectToLogin}>Go to login</Button>
+      </Alert>
     );
   }
 }
